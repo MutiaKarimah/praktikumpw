71be63e7d802f6f9aa5aa287154a3894f7a87893
mutekarim <43401048+mutekarim@users.noreply.github.com>
2018-10-15 05:41:24 +0700

    Add files via upload

---
+++ Proyek Toko/index.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+<title> MGI's GAMESTORE</title>
+ <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<header id="top-header" >
+<div class="header">
+<h1> <a href="#" class="NamaToko"  > MGI's Gamestore </a> </h1>
+
+	
+
+
+</div>
+<hr class="line"><hr class="lineShading">
+</header>
+
+
+
+<nav class="menu">
+		<div class="sidebar">
+		
+			
+   		 		<div class="search">
+	  			   <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+				   <input type="submit" value="">
+	  			</div>
+				
+				<div class=info>
+  <div class="foto">
+  </div>
+<p> - </p>
+ </div>
+			
+   		 	</div>
+	<div class="login">
+	<form action="/action_page.php">
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+</form>
+	</div>
+</nav>
+<div id="hot-deal" class="section">		
+						<a style="margin-left:10%;margin-top:20%;"class="primary-btn cta-btn" href="#">Shop now</a>
+	
+		</div>
+
+		
+		
+		<div>	
+						<div class="shop">
+							<div class="shop-img">
+								<img src="images/Dota2.jpg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>CD<br>Game</h3>
+								<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+
+						<div class="shop">
+							<div class="shop-img">
+								<img src="images/Windows.jpg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>CD<br>Software</h3>
+								<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+
+						<div class="shop">
+							<div class="shop-img">
+								<img src="images/Pala.jpg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>CD<br>Game 2</h3>
+								<a href="#" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div>
+
+			</div>
+			
+		
+
+
+</body>
+
+</html>
