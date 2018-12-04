ac603a222f7b7a23f2e074f413f9662c6a0d6142
mutekarim <mutyakarim@gmail.com>
2018-12-05 00:21:22 +0700

    new register

---
+++ register.php
@@ -0,0 +1,105 @@
+<html>
+	<head>
+		<title> MGI's GAMESTORE</title>
+		<link href="css/style.css" rel="stylesheet">
+		
+	</head>
+	<?php
+session_start();
+if(isset($_SESSION["username"])){
+	echo header("location:siap.php");
+}else{
+;
+ 
+}
+?>
+<body>
+	<div class="sidebar">
+   		 		<div class="search">
+	  			   <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+				   <input type="submit" value="">
+	  			</div>
+   		 	</div>
+		<header id="top-header" >
+			<div class="header">
+				<h1> <a href="#" class="NamaToko"  > MJA's Gamestore </a> </h1>
+			</div>
+			
+			<hr class="line"><hr class="lineShading">
+		</header>
+
+		<hr class="lineShading">	
+		<div class="button-up">
+		<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+		</div>
+		<aside>
+		<div class="menu">
+			<div class="login">
+				<form method="post" action="login.php">					
+						<label for="username"><b>Username</b></label>
+						<input type="text" placeholder="Enter Username" name="iusername" required></input>
+						<label for="password"><b>Password</b></label>
+						<input type="password" placeholder="Enter Password" name="ipassword" required></input>
+						<button type="submit" value="Login">Login</button>
+						<label><input type="checkbox" checked="checked" name="remember"> Remember me |						</label>
+						<label><a href="#"> Sign Up </label></a>					
+				</form>
+			</div>
+			<hr class="line">
+			<a href="#"><h3>Cara Memesan</h3></a>
+			<hr class="line">
+			<a href="#"><h3>List PC Game</h3></a>
+			<hr class="line">
+			<a href="#"><h3>FAQ</h3></a>
+			<hr class="line">
+			<a href="#"><h3>Support</h3></a>
+			<hr class="lineShading">
+			
+		
+		
+		</aside>
+		
+		<div class="container">	
+		 <h2 class="title"><span>Register</span></h2>
+				//* Letakin Formnya disini *//		
+			
+			</div>
+</div>
+		
+<footer>
+<hr class="lineShading">
+<div class="footer1">
+<i class="far fa-home"></i>Alamat Toko Pusat<br>
+<i class="far fa-home"></i>Alamat Toko Cabang #1<br>
+<i class="far fa-home"></i>Alamat Toko Cabang #2<br>
+<i class="far fa-home"></i>Alamat Toko Cabang #3<br>
+<i class="far fa-home"></i>Alamat Toko Cabang #4<br>
+</div>
+<div class="footer2">
+No. Telepon
+E-MAIL
+</div>
+<div class="contact">
+Website ini dibuat oleh MGI studio untuk <br>memudahka pembelian DVD diseluruh Indonesia.
+</div>
+</footer>
+</body>
+<script>
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
+</html>
+
